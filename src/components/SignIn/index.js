a55cf58acb452fbbf19bf64a277e19a25f2a6dbe
Eldar Kurmanaliev <ekurmanaliev50@gmail.com>
2020-07-31 21:45:50 +0300

    Created signin form

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
-
+import { Arcticle, NameField, Button, Input } from './styles';
 const Signin = () => {
   return (
-    
+    <Arcticle>
+      <fieldset id='sign_up' className='fieldset'>
+        <legend className='legend'>SIGN IN</legend>
+        <div>
+          <NameField>Email</NameField>
+          <Input type='email' />
+          <NameField>Password</NameField>
+          <Input type='password' />
+          <Button>Sign In</Button>
+        </div>
+      </fieldset>
+    </Arcticle>
   );
 };
 export default Signin;
--- src/components/SignIn/styles.js
+++ src/components/SignIn/styles.js
@@ -1,6 +1,43 @@
 import styled from 'styled-components';
 
-export const Arcticle = styled.article`
+export const Arcticle = styled.div`
   border-radius: 0.5rem;
-  max-width: 300px;
+  padding: 24px;
+  max-width: 500px;
+  min-height: 320px;
+  box-shadow: 4px 4px 8px 0px rgba(0, 0, 0, 0.3);
+  font-size: 16px;
+  margin: auto auto;
+  margin-top: 100px;
+  border: none;
+  .fieldset {
+    border: none;
+  }
+  .legend {
+    font-size: 24px;
+  }
+`;
+export const NameField = styled.span`
+  display: block;
+  margin-top: 16px;
+  margin-bottom: 8px;
+`;
+export const Button = styled.button`
+  display: block;
+  width: auto;
+  padding: 8px 16px;
+  margin: 16px auto;
+  background: rgba(0, 0, 0, 0);
+  border: 1px solid black;
+  font-size: 20px;
+  &:hover {
+    color: white;
+    cursor: pointer;
+  }
+`;
+export const Input = styled.input`
+  background: rgba(0, 0, 0, 0);
+  border: 1px solid black;
+  width: 200px;
+  height: 32px;
 `;
